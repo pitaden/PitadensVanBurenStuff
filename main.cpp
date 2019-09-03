7f88e609d74a0135797fdfca16fa516766358b33
pitaden <32023715+pitaden@users.noreply.github.com>
2019-09-03 09:56:22 -0500

    Placeholder file
    I'll edit this Eventually(tm)

---
+++ main.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+
+class FileCrt{ //critter
+	char extension[] = "crt"
+	char bodyType[] = "Str";
+	char gender = "M";
+	//99.9% todo
+}
+
+class FileTre{ //map
+	char extension[] = "tre";
+}
+
+class FileB3d{ //UNCONF model
+	char extension[] - "b3d";
+}
+class FileSkel{ //UNCONF model skeleton
+	char extension[] - "skel";
+}
+
+int main(){
+	return 0;
+}
